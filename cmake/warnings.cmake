81737ea87f9799ffbbe3ef796070b182ebf349c1
Harper <harper.stoeckle@gmail.com>
2020-08-26 16:32:42 -0600

    Use -Wconversion instead of just -Wsign-conversion

--- cmake/warnings.cmake
+++ cmake/warnings.cmake
@@ -10,13 +10,14 @@ function(enable_compile_warnings target)
 	)
 	if (${TARGET_WARNING_OPTION_NAME})
 		set(MSVC_WARNINGS /W4)
-		set(GCC_WARNINGS -pedantic -Wall -Wextra -Wcast-align -Wcast-qual
-			-Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2
-			-Winit-self -Wlogical-op -Wmissing-declarations
-			-Wmissing-include-dirs -Wnoexcept -Wold-style-cast
-			-Woverloaded-virtual -Wredundant-decls -Wshadow
-			-Wsign-conversion -Wsign-promo -Wstrict-null-sentinel
-			-Wstrict-overflow=5 -Wswitch-default -Wundef -Wno-unused
+		set(GCC_WARNINGS -pedantic -Wall -Wextra -Wcast-align
+			-Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization
+			-Wformat=2 -Winit-self -Wlogical-op
+			-Wmissing-declarations -Wmissing-include-dirs
+			-Wnoexcept -Wold-style-cast -Woverloaded-virtual
+			-Wredundant-decls -Wshadow -Wconversion -Wsign-promo
+			-Wstrict-null-sentinel -Wstrict-overflow=5
+			-Wswitch-default -Wundef -Wno-unused
 			-Wimplicit-fallthrough=5 -Wswitch-enum
 		)
 
