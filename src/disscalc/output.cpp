cbfbfc70341c5ec7e0293a3b8a687cebf2e9d1ef
Harper <harper.stoeckle@gmail.com>
2020-10-16 18:25:04 -0600

    Generate usage message from file
    Previously, the usage message was specified using a normal string
    literal in `disscalc/output.cpp`. Unfortunately, formatting the contents
    of a string literal is not very effective, so I would typically edit the
    usage message in a file, then copy it into a string literal in the
    source file.
    
    Clearly, it would be much more convenient if that step could be done
    automatically. Cmake has several features that would facilitate this:
    
    - `file(READ)` can be used to read the usage message into a variable.
    - `configure_file` can be used to configure a source file with the
      contents of that variable (being the contents of the file) inserted.
    
    The problem with these is that all of this is done at configure time
    rather than build time, and cmake doesn't know that the file containing
    the usage message is a dependency for the target, so cmake has to be
    manually reconfigured every time that file changes.
    
    As it turns out, cmake has a feature for this exact thing: the property
    `CMAKE_CONFIGURE_DEPENDS`. This contains a list of files that are
    dependencies to the configuration of cmake itself. The usage message
    file can be specified as a dependency that way, and cmake will
    automatically reconfigure whenever it is changed.

--- src/disscalc/output.cpp
+++ src/disscalc/output.cpp
@@ -1,5 +1,7 @@
 #include "disscalc/output.hpp"
 
+#include <generated/usage.hpp>
+
 #include <iomanip>
 
 namespace disscalc
@@ -61,51 +63,6 @@ void print_generic_error(std::ostream& out, std::string_view error)
 
 void print_usage_message(std::ostream& out)
 {
-	constexpr std::string_view usage_message =
-		"Usage: disscalc [--help] [--output=<file>] [--format=<format>]\n"
-		"                [--start=<number>] [--delta=<number>] [--quantity=<number>]\n"
-		"                [--end=<number>] [-x <number>...] -p <number>... -a <number>...\n"
-		"\n"
-		"Generate a dissonance curve for the given timbre.\n"
-		"\n"
-		"Options:\n"
-		"  -h, --help                         Display this usage and exit.\n"
-		"  -o <file>, --output=<file>         Send output to the specified file.\n"
-		"  -f <format>, --format=<format>     Use the specified output format. The\n"
-		"                                     available formats are csv and tsv.\n"
-		"  -s <number>, --start=<number>      Use the given positive number as the\n"
-		"                                     (inclusive) lower bound of intervals\n"
-		"                                     tested. The default is 1.0.\n"
-		"  -d <number>, --delta=<number>      Use the given positive number as the\n"
-		"                                     difference between each interval tested.\n"
-		"                                     The default is 0.01.\n"
-		"  -e <number>, --end=<number>        Use the given positive number as the\n"
-		"                                     (inclusive) upper bound of intervals\n"
-		"                                     tested. The default is 2.0.\n"
-		"  -x <number>...                     Along with the normal range of numbers,\n"
-		"                                     also compute dissonances for the intervals\n"
-		"                                     specified in this option.\n"
-		"  -p <number>...                     Specify the frequencies of the stationary\n"
-		"                                     partials. If this option is given multiple\n"
-		"                                     times, the partials are concatenated in\n"
-		"                                     the order they were given. Each must be a\n"
-		"                                     positive number.\n"
-		"  -a <number>...                     Specify the amplitudes of the stationary\n"
-		"                                     partials. If this option is given multiple\n"
-		"                                     times, the amplitudes are concatenated in\n"
-		"                                     the order they were given. Each must be a\n"
-		"                                     positive number. The total number of\n"
-		"                                     amplitudes must match the number of\n"
-		"                                     partials, and each nth amplitude will be\n"
-		"                                     matched with the nth partial.\n"
-		"  -P <number>...                     Specify the frequencies of the mobile\n"
-		"                                     partials. If not provided, use the\n"
-		"                                     stationary frequencies by default.\n"
-		"  -A <number>...                     Specify the frequencies of the mobile\n"
-		"                                     amplitudes. If not provided, use the\n"
-		"                                     stationary amplitudes by default. The\n"
-		"                                     number of mobile amplitudes must be equal\n"
-		"                                     to the number of mobile frequencies.\n";
 	out << usage_message;
 }
 }
